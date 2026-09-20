0221752c42e20e2df3b3d0577c5495da72898662
abdulhamidxamitjonov-ship-it <abdulhamidxamitjonov@gmail.com>
2026-09-20 16:59:02 +0500

    Add files via upload

--- app/states/admin_content.py
+++ app/states/admin_content.py
@@ -1,4 +1,15 @@
-from aiogram.fsm.state import State,StatesGroup
-class MovieAddState(StatesGroup): waiting_video=State(); waiting_info=State(); waiting_poster=State()
-class SeriesAddState(StatesGroup): waiting_video=State(); waiting_info=State(); waiting_poster=State()
-class MandatoryAddState(StatesGroup): waiting_chat_id=State(); waiting_url=State()
+from aiogram.fsm.state import State, StatesGroup
+
+class MovieAddState(StatesGroup):
+    waiting_video = State()
+    waiting_info = State()
+
+class SeriesAddState(StatesGroup):
+    waiting_video = State()
+    waiting_info = State()
+
+class MandatoryAddState(StatesGroup):
+    waiting_url = State()
+
+class VipPurchaseState(StatesGroup):
+    waiting_receipt = State()
